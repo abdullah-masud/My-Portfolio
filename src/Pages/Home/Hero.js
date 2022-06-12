b06a52339e6257ffccf96d883c8834133aa7eb1c
Abdullah Al Masud <masudctg00@gmail.com>
2022-06-12 19:59:29 +0600

    resume updated

--- src/Pages/Home/Hero.js
+++ src/Pages/Home/Hero.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-scroll';
 import image from '../../assets/img/person.png'
-import resume from '../../assets/portfolio/Resume-Masud.pdf'
+import resume from '../../assets/resume/Resume-Masud.pdf'
 
 const Hero = () => {
     return (
