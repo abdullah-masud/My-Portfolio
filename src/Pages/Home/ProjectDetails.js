b2bf18b4f8b2e74b043a49bfac6950fbaece9fde
Abdullah Al Masud <masudctg00@gmail.com>
2022-06-12 12:15:03 +0600

    carousel ghost

--- src/Pages/Home/ProjectDetails.js
+++ src/Pages/Home/ProjectDetails.js
@@ -14,22 +14,22 @@ const ProjectDetails = () => {
                         <div id="slide1" class="carousel-item relative w-full">
                             <img src={image1} class="w-full" alt='' />
                             <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                <a href="#slide4" class="btn btn-circle">❮</a>
-                                <a href="#slide2" class="btn btn-circle">❯</a>
+                                <a href="#slide4" class="btn btn-circle btn-ghost">❮</a>
+                                <a href="#slide2" class="btn btn-circle btn-ghost">❯</a>
                             </div>
                         </div>
                         <div id="slide2" class="carousel-item relative w-full">
                             <img src={image2} class="w-full" alt='' />
                             <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                <a href="#slide1" class="btn btn-circle">❮</a>
-                                <a href="#slide3" class="btn btn-circle">❯</a>
+                                <a href="#slide1" class="btn btn-circle btn-ghost">❮</a>
+                                <a href="#slide3" class="btn btn-circle btn-ghost">❯</a>
                             </div>
                         </div>
                         <div id="slide3" class="carousel-item relative w-full">
                             <img src={image3} class="w-full" alt='' />
                             <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                <a href="#slide2" class="btn btn-circle">❮</a>
-                                <a href="#slide4" class="btn btn-circle">❯</a>
+                                <a href="#slide2" class="btn btn-circle btn-ghost">❮</a>
+                                <a href="#slide4" class="btn btn-circle btn-ghost">❯</a>
                             </div>
                         </div>
                     </div>
