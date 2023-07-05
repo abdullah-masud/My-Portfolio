627eb422d5b919834a8152c59d77db6b45195c14
Abdullah Al Masud <masudctg00@gmail.com>
2023-07-05 22:29:04 +0600

    new project added to wordpress

--- src/Pages/Shared/wordpressData.js
+++ src/Pages/Shared/wordpressData.js
@@ -3,6 +3,7 @@ import bounty from '../../assets/img/projects/wp/bounty.png'
 import agenda from '../../assets/img/projects/wp/agenda.png'
 import digitalagency from '../../assets/img/projects/wp/digital-agency.png'
 import naxos from '../../assets/img/projects/wp/naxos.png'
+import charity from '../../assets/img/projects/wp/charity.png'
 
 
 const wordpressData = [
@@ -55,6 +56,16 @@ const wordpressData = [
         description: 'Get the new App for iPhone & Android',
         fullDescription: "Consequat enim mollis mauris vulputate phasellus neque, eros turpis et eu adipiscing id tempor, nascetur elit vitae molestie pulvinar viverra ut eu lectus mi velit cras integer tristique aliquam proin porta tellus elit neque mi velit sed pharetra morbi proin.",
         liveSiteHref: 'https://intechblog.com/naxos/',
+        technologies: 'WordPress, Elementor Pro'
+    },
+    {
+        id: '6',
+        image1: charity,
+        name: 'African Aid Foundation',
+        category: 'wordpress',
+        description: 'The African Aid Foundation, a non-profit organization',
+        fullDescription: "The African Aid Foundation, a non-profit organization under Tanzanian Law, was established in July 2019 to improve the welfare of the people through better education, healthcare, environmental protection, information delivery, and technology application.",
+        liveSiteHref: 'https://africanaidfoundation.or.tz/',
         technologies: 'WordPress, Elementor Pro'
     },
 
