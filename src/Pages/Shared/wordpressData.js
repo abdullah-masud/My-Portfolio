ee7eb9a349dca72bbacc939262b1a6fe12e4ab1d
Abdullah Al Masud <masudctg00@gmail.com>
2023-08-15 02:34:08 +0600

    wordpress live link changed

--- src/Pages/Shared/wordpressData.js
+++ src/Pages/Shared/wordpressData.js
@@ -8,13 +8,24 @@ import charity from '../../assets/img/projects/wp/charity.png'
 
 const wordpressData = [
     {
+        id: '6',
+        image1: charity,
+        name: 'African Aid Foundation',
+        category: 'wordpress',
+        description: 'The African Aid Foundation, a non-profit organization',
+        fullDescription: "The African Aid Foundation, a non-profit organization under Tanzanian Law, was established in July 2019 to improve the welfare of the people through better education, healthcare, environmental protection, information delivery, and technology application.",
+        liveSiteHref: 'https://africanaidfoundation.or.tz/',
+        technologies: 'WordPress, Elementor Pro'
+    },
+    {
         id: '1',
         image1: delectus,
         name: 'Delectus',
         category: 'wordpress',
         description: 'Leading Internet Marketing Agency Based on Florida',
         fullDescription: "Consequat enim mollis mauris vulputate phasellus neque, eros turpis et eu adipiscing id tempor, nascetur elit vitae molestie pulvinar viverra ut eu lectus mi velit cras integer tristique aliquam proin porta tellus elit neque mi velit sed pharetra morbi proin.",
-        liveSiteHref: 'https://intechblog.com/delectus/',
+        // liveSiteHref: 'https://intechblog.com/delectus/',
+        liveSiteHref: 'https://drive.google.com/file/d/1NoZU7JlKFqFi0tVK_tUaaiBwKadAyQxD/view?usp=sharing',
         technologies: 'WordPress, Elementor Pro'
     },
     {
@@ -24,7 +35,8 @@ const wordpressData = [
         category: 'wordpress',
         description: 'Discover Our Proven Formula To Get Success',
         fullDescription: "Consequat enim mollis mauris vulputate phasellus neque, eros turpis et eu adipiscing id tempor, nascetur elit vitae molestie pulvinar viverra ut eu lectus mi velit cras integer tristique aliquam proin porta tellus elit neque mi velit sed pharetra morbi proin.",
-        liveSiteHref: 'https://intechblog.com/digitalagency/',
+        // liveSiteHref: 'https://intechblog.com/digitalagency/',
+        liveSiteHref: 'https://drive.google.com/file/d/18Pws5uqsCRA5RDCip0x7SgiXj4iH70L8/view?usp=sharing',
         technologies: 'WordPress, Elementor Pro'
     },
 
@@ -35,7 +47,8 @@ const wordpressData = [
         category: 'wordpress',
         description: 'BEST OFFICE SUPPLIES',
         fullDescription: "Consequat enim mollis mauris vulputate phasellus neque, eros turpis et eu adipiscing id tempor, nascetur elit vitae molestie pulvinar viverra ut eu lectus mi velit cras integer tristique aliquam proin porta tellus elit neque mi velit sed pharetra morbi proin.",
-        liveSiteHref: 'https://intechblog.com/agenda/',
+        // liveSiteHref: 'https://intechblog.com/agenda/',
+        liveSiteHref: 'https://drive.google.com/file/d/18Y01H4NKtSvFs7GNapFA_Bhw-v2idncU/view?usp=sharing',
         technologies: 'WordPress, Elementor Pro'
     },
     {
@@ -45,7 +58,8 @@ const wordpressData = [
         category: 'wordpress',
         description: 'HELPS PEOPLE LIFE',
         fullDescription: "Consequat enim mollis mauris vulputate phasellus neque, eros turpis et eu adipiscing id tempor, nascetur elit vitae molestie pulvinar viverra ut eu lectus mi velit cras integer tristique aliquam proin porta tellus elit neque mi velit sed pharetra morbi proin.",
-        liveSiteHref: 'https://intechblog.com/bounty/',
+        // liveSiteHref: 'https://intechblog.com/bounty/',
+        liveSiteHref: 'https://drive.google.com/file/d/1QM_GoUQ9mf_05xyUm69Jx6bseGWZ2jGV/view?usp=sharing',
         technologies: 'WordPress, Elementor Pro'
     },
     {
@@ -55,19 +69,11 @@ const wordpressData = [
         category: 'wordpress',
         description: 'Get the new App for iPhone & Android',
         fullDescription: "Consequat enim mollis mauris vulputate phasellus neque, eros turpis et eu adipiscing id tempor, nascetur elit vitae molestie pulvinar viverra ut eu lectus mi velit cras integer tristique aliquam proin porta tellus elit neque mi velit sed pharetra morbi proin.",
-        liveSiteHref: 'https://intechblog.com/naxos/',
+        // liveSiteHref: 'https://intechblog.com/naxos/',
+        liveSiteHref: 'https://drive.google.com/file/d/1ntCoH_AD7YK0U5TXWMCf5Hpfte7CYBhz/view?usp=sharing',
         technologies: 'WordPress, Elementor Pro'
     },
-    {
-        id: '6',
-        image1: charity,
-        name: 'African Aid Foundation',
-        category: 'wordpress',
-        description: 'The African Aid Foundation, a non-profit organization',
-        fullDescription: "The African Aid Foundation, a non-profit organization under Tanzanian Law, was established in July 2019 to improve the welfare of the people through better education, healthcare, environmental protection, information delivery, and technology application.",
-        liveSiteHref: 'https://africanaidfoundation.or.tz/',
-        technologies: 'WordPress, Elementor Pro'
-    },
+
 
 ]
 
