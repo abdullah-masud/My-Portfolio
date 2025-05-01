fec18003f17f5aaa0757df57634ec9cfad31354c
Abdullah Al Masud <masudctg00@gmail.com>
2025-05-01 10:52:43 +0800

    new skills added

--- src/Pages/Home/Skills.js
+++ src/Pages/Home/Skills.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import html from '../../assets/img/skills/html5.png';
-import css from '../../assets/img/skills/css3.png';
+// import html from '../../assets/img/skills/html5.png';
+// import css from '../../assets/img/skills/css3.png';
 import js from '../../assets/img/skills/js.png';
 import react from '../../assets/img/skills/reactjs.png';
 import expressJs from '../../assets/img/skills/expressJs.png'
@@ -9,6 +9,8 @@ import mongodb from '../../assets/img/skills/mongodb.svg';
 import firebase from '../../assets/img/skills/firebase.svg';
 import wordpress from '../../assets/img/skills/wordpress.png';
 import elementor from '../../assets/img/skills/elementor.png';
+import python from '../../assets/img/skills/python.png';
+import flask from '../../assets/img/skills/flask.png';
 
 const Skills = () => {
     return (
@@ -16,16 +18,10 @@ const Skills = () => {
             <div className='container max-w-7xl mx-auto  px-2 lg:px-0'>
                 <div className='grid grid-cols-10 md:grid-flow-col'>
                     <div className='flex items-center justify-center'>
-                        <img className='lg:h-20' src={html} alt='' />
-                    </div>
-                    <div className='flex items-center justify-center'>
-                        <img className='lg:h-20' src={css} alt='' />
+                        <img className='lg:h-20' src={react} alt='' />
                     </div>
                     <div className='flex items-center justify-center'>
                         <img className='lg:h-20' src={js} alt='' />
-                    </div>
-                    <div className='flex items-center justify-center'>
-                        <img className='lg:h-20' src={react} alt='' />
                     </div>
                     <div className='flex items-center justify-center'>
                         <img className='lg:h-20' src={expressJs} alt='' />
@@ -44,6 +40,12 @@ const Skills = () => {
                     </div>
                     <div className='flex items-center justify-center'>
                         <img className='lg:h-20' src={elementor} alt='' />
+                    </div>
+                    <div className='flex items-center justify-center'>
+                        <img className='lg:h-20' src={python} alt='' />
+                    </div>
+                    <div className='flex items-center justify-center'>
+                        <img className='lg:h-20' src={flask} alt='' />
                     </div>
                 </div>
             </div>
