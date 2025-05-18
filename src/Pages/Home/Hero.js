eaf2ab26e6718ee0fe0853d77b72ffb235584911
Abdullah Al Masud <masudctg00@gmail.com>
2025-05-19 01:33:05 +0800

    responsive

--- src/Pages/Home/Hero.js
+++ src/Pages/Home/Hero.js
@@ -15,11 +15,10 @@ const Hero = () => {
                         <p className='text-lg text-accent text-md mb-[22px] mx-auto' >
                             Hi, I'm Abdullah Al Masud! 👋
                         </p>
-                        <h1 className='text-2xl  text-white font-primary leading-[44px] md:text-5xl md:leading-tight lg:text-6xl lg:leading-[1.2] font-bold md:tracking-[-2px] lg:mb-0 mb-3 lg:text-start text-center lg:px-0 px-20 '>
+                        <h1 className='text-2xl text-white font-primary leading-[32px] md:text-5xl md:leading-tight lg:text-6xl lg:leading-[1.2] font-bold md:tracking-[-2px] lg:mb-0 mb-3 lg:text-start text-center lg:px-0 px-20'>
                             Software Engineer & Full Stack Developer
-
-
                         </h1>
+
 
                         <div className='flex gap-x-3 lg:mt-0 mt-5 lg:flex-row   mx-auto '>
                             <Link to='portfolio' activeClass='active'
